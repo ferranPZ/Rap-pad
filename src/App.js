df80bb8752182e40e55fabf65438052e2c4174bd
ferranPZ <lucas.pendola.z@gmail.com>
2020-10-29 21:58:44 -0300

    Fix

--- src/App.js
+++ src/App.js
@@ -4,69 +4,71 @@ import RapPad from './components/RapPad'
 
 
 
+
+
 import './styles/App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
 
-    this.state={
-        frecuencia:15,
-        palabras:[],
-        config:false,
-        pad:true
-        
+    this.state = {
+      frecuencia: 15,
+      palabras: [],
+      config: false,
+      pad: true
+
     }
-    this.set=this.set.bind(this);
-    this.turn=this.turn.bind(this);
-}
-
-
-set(fq,pb){
-  this.setState({
-    frecuencia:fq,
-    palabras:pb
-  })
-   this.turn()
-}
-
-turn(){
-  if(this.state.config===true){
-    this.setState({
-      config:false,
-      pad:true
-    })
-  }else{
-    this.setState({
-      config:true,
-      pad:false
-    })
+    this.set = this.set.bind(this);
+    this.turn = this.turn.bind(this);
   }
-}
+
+
+  set(fq, pb) {
+    this.setState({
+      frecuencia: fq,
+      palabras: pb
+    })
+    this.turn()
+  }
+
+  turn() {
+    if (this.state.config === true) {
+      this.setState({
+        config: false,
+        pad: true
+      })
+    } else {
+      this.setState({
+        config: true,
+        pad: false
+      })
+    }
+  }
 
 
   render() {
     return (
       <div className="App">
 
- 
-      
+
+
 
 
 
         <div className="App-intro">
 
-{/*
+          {/*
           {(this.state.Showfig)?  null : <RapPad handleShowfig={this.handleShowfig}/>}
           {(this.state.Showfig)? <Config/> : null}
-*/}         
+*/}
 
- {(this.state.config)? <Config set={this.set}/> : null}
-{(this.state.pad)? <RapPad fq={this.state.frecuencia} pb={this.state.palabras} turn={this.turn}/> : null}
+          {(this.state.config) ? <Config set={this.set} /> : null}
+          {(this.state.pad) ? <RapPad fq={this.state.frecuencia} pb={this.state.palabras} turn={this.turn} /> : null}
 
 
 
-          
+
         </div>
       </div>
     );
--- src/components/RapPad.js
+++ src/components/RapPad.js
@@ -4,124 +4,125 @@ import palabras from '../words/palabras.json';
 
 
 class RapPad extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        this.state={
-            palabra:"",
-            palabras:this.props.pb,
-            frecuencia:this.props.fq,
-            tiempo:"",
-            rimas:[]
+        this.state = {
+            palabra: "",
+            palabras: this.props.pb,
+            frecuencia: this.props.fq,
+            tiempo: "",
+            rimas: []
         }
-        this.handleChange=this.handleChange.bind(this);
-        this.removeFirst=this.removeFirst.bind(this);
-        this.addRandomWord= this.addRandomWord.bind(this);
-        this.getRhymes=this.getRhymes.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.removeFirst = this.removeFirst.bind(this);
+        this.addRandomWord = this.addRandomWord.bind(this);
+        this.getRhymes = this.getRhymes.bind(this);
     }
 
 
 
-    config(){
+    config() {
         this.props.turn();
         console.log("handleShowfig triggered");
     }
 
-    handleChange(e){
+    handleChange(e) {
         console.log(e.target.value);
         this.setState({
             [e.target.name]: e.target.value
         })
     }
 
-    removeFirst(){
-            this.setState({
-                palabras:this.state.palabras.filter((e,i)=>{ return i !== 0 })
-            })
-       
-      }
+    removeFirst() {
+        this.setState({
+            palabras: this.state.palabras.filter((e, i) => { return i !== 0 })
+        })
 
-    getRhymes(){
-        console.log("palabra: "+this.state.palabras[0]);
+    }
+
+    getRhymes() {
+        console.log("palabra: " + this.state.palabras[0]);
         console.log("rimas: ")
 
         var list = [];
-        var max=0;
+        var max = 0;
 
 
-        fetch('http://rhymebrain.com/talk?function=getRhymes&word='+this.state.palabras[0]+'&lang=es&includePron=1')
-            .then(function(response) {
+        fetch('http://rhymebrain.com/talk?function=getRhymes&word=' + this.state.palabras[0] + '&lang=es&includePron=1')
+            .then(function (response) {
+                console.log("rimas:" + response.json())
                 return response.json();
             })
-            .then(function(myJson) { 
+            .then(function (myJson) {
 
-                if(myJson.length>=100)
-                {max=100;}else{max=myJson.length}
+                if (myJson.length >= 100) { max = 100; } else { max = myJson.length }
 
-                for(var j=0;j<max;j++){
+                for (var j = 0; j < max; j++) {
                     list.push(myJson[j].word);
 
-                   
-                 }
-             });
 
-             this.setState({
-                rimas:list
-             })
-             
-             console.log(this.state.rimas)
-            
-
-        }
-
-
-        clearRimas(){
-            this.setState({
-                rimas:[]
+                }
             })
-        }
-    
+            .catch(error => { console.log("error:" + error); });
+
+        this.setState({
+            rimas: list
+        })
+
+        console.log(this.state.rimas)
 
 
-    componentDidMount(){
+    }
 
-        if(this.state.palabras.length===0){ 
-        this.addRandomWord()
+
+    clearRimas() {
+        this.setState({
+            rimas: []
+        })
+    }
+
+
+
+    componentDidMount() {
+
+        if (this.state.palabras.length === 0) {
+            this.addRandomWord()
         }
         const intervalo = this.state.frecuencia;
         var time = intervalo;
-        var i=0;
+        var i = 0;
 
-        window.setInterval( function(){
+        window.setInterval(function () {
 
-             
-            
+
+
             this.setState({
-                tiempo : time
+                tiempo: time
             })
             time--;
-            if(time===-1){
-                time=intervalo;
+            if (time === -1) {
+                time = intervalo;
 
-               
-                
+
+
             }
 
 
-            
-            if(this.state.tiempo===this.state.frecuencia){
 
-                
-                if(i===0){
-                this.getRhymes();
-                
-                
+            if (this.state.tiempo === this.state.frecuencia) {
 
-                }else{
+
+                if (i === 0) {
+                    this.getRhymes();
+
+
+
+                } else {
                     this.clearRimas()
                     this.removeFirst();
-                    
-                    if(this.state.palabras.length===0){
+
+                    if (this.state.palabras.length === 0) {
                         this.addRandomWord()
                     }
                     this.getRhymes();
@@ -130,115 +131,115 @@ class RapPad extends Component {
                 i++
             }
 
-          
-            }.bind(this),1000);
-        
+
+        }.bind(this), 1000);
+
     }
 
 
-    addRandomWord(){
-        
+    addRandomWord() {
+
         var value = Math.floor((Math.random() * 249387) + 1);
         this.setState({
-            palabras:[...this.state.palabras,palabras[value]]
+            palabras: [...this.state.palabras, palabras[value]]
         })
 
         console.log(palabras[value]);
     }
 
-    
 
-  render() {
 
-    var x=10;
-    const array = this.state.rimas.map(
-      (rimas,i)=>{ 
+    render() {
+
+        var x = 10;
+        const array = this.state.rimas.map(
+            (rimas, i) => {
+                return (
+
+                    <div className="col-4" key={i}>
+                        {rimas}
+
+
+                    </div>
+
+
+
+                )
+            })
+
+
+
+
         return (
 
-          <div className="col-4" key={i}>
-            {rimas}
+            <div className="App">
 
-          
-          </div>
+                <div className="container">
 
+                    <div className="card">
 
-
-        )
-      })
+                        <div className="card-header">
 
 
-        
-      
-    return (
+                            <h5 className="card-title mt-3">Entrenador de Freestyle Rap!</h5>
 
-      <div className="App">
+                            <button type="button" className="btn btn-outline-secondary config mb-4" onClick={this.config.bind(this)} >Configurar</button>
 
-        <div className="container">
+                        </div>
 
-            <div className="card">
+                        <div className="card-body " >
 
-                <div className="card-header">
-                
-                    
-                    <h5 className="card-title mt-3">Entrenador de Freestyle Rap!</h5>
-                   
-                    <button type="button" className="btn btn-outline-secondary config mb-4" onClick={this.config.bind(this)} >Configurar</button>
+                            <div className="row word">
+                                <p className="col"> {this.state.palabras[0]}</p>
+                            </div>
+                            <div class="row progress mb-4">
+                                <div class="progress-bar" style={{ width: ((this.state.tiempo * 100) / this.state.frecuencia) + "%" }} role="progressbar" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100">
+                                    <p className="seg">{this.state.tiempo}</p>
+                                </div>
+                            </div>
 
+
+
+
+
+                            <div className="row rimas">
+                                {array}
+                            </div>
+
+
+
+
+
+
+                            <div className="row music mt-3">
+
+
+                                <iframe width="100%" height="200" src={"https://www.youtube.com/embed?listType=playlist&list=PLXhSAMqlo7Ii0SlLoxFqe4DbVZfTIVuGl&autoplay=1&loop=1&index=" + 3} frameBorder="0" ></iframe>
+
+
+                            </div>
+
+
+
+
+                        </div>
+
+                        <div className="card-footer text-muted">
+                            copyright
                 </div>
-                
-                <div  className="card-body " >
-                
-                <div className="row word">
-                   <p className="col"> {this.state.palabras[0]}</p> 
-                </div>                
-                    <div class="row progress mb-4">
-                        <div class="progress-bar" style={{width: ((this.state.tiempo*100)/this.state.frecuencia)+"%" }} role="progressbar" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100">
-                        <p className="seg">{this.state.tiempo}</p>
+
+
+
                     </div>
-                </div>
-        
-               
-             
-
-           
-                    <div className="row rimas">
-                        {array}
-                    </div>
-
-
-
-
-               
-
-                <div className="row music mt-3">
-
-
-                <iframe width="100%" height="200" src={"https://www.youtube.com/embed?listType=playlist&list=PLXhSAMqlo7Ii0SlLoxFqe4DbVZfTIVuGl&autoplay=1&loop=1&index="+3} frameBorder="0" ></iframe>
-
-                
-                </div>
-
-
-
-
-                </div>
-
-                <div className="card-footer text-muted">
-                        copyright
                 </div>
 
 
 
             </div>
-        </div>
-       
 
-        
-      </div>
+        );
 
-    );
-
-  }
+    }
 }
 
 export default RapPad;
